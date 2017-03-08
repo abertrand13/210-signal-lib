40d1fe67133ebb4c3eaabe90393bfb40b9cd378d
Alex Bertrand <alex.bertrand13@gmail.com>
2017-03-07 16:42:14 -0800

    Initial commit

---
+++ Signals.cpp
@@ -0,0 +1,20 @@
+#include "Signals.h"
+
+void sendSignal(signal s) {
+	Serial.println(s);
+	// Serial.write('h');
+	// Serial.println("Transmitting");
+}
+
+signal receiveSignal() {
+	enum signal sig;
+	
+	if(Serial.available() > 0) {
+		// sig = Serial.read();
+		char x = Serial.read();
+	} else {
+		sig = NONE;
+	}
+	Serial.read(); // read in newline
+	return sig;
+}
---
+++ Signals.h
@@ -0,0 +1,27 @@
+#include "Arduino.h"
+
+enum signal {
+	NONE,
+	FOUND_IR,
+	ALIGNED,
+	NEXT_LEFT,
+	NEXT_MID,
+	NEXT_RIGHT,
+	NEXT_REFILL,
+	READY2FIRE,
+	REFILL_DONE
+};
+
+// not strictly related to signalling, but location sync
+// is a major part of what we're doing here so it feels
+// right to include this
+enum location {
+	REFILL,
+	GOAL_LEFT,
+	GOAL_MID,
+	GOAL_RIGHT
+};
+
+void sendSignal(signal s);
+
+signal receiveSignal();
