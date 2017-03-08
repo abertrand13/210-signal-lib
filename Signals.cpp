c56a6dba5617dc7b36aac91bbac21bc6af240c67
Eni Asebiomo <eniolufe@stanford.edu>
2017-03-07 21:08:44 -0800

    working signal code

--- Signals.cpp
+++ Signals.cpp
@@ -1,33 +1,13 @@
 #include "Signals.h"
 
-void sendSignal(signal s) {
-	Serial.println(s);
-	// Serial.write('h');
-	// Serial.println("Transmitting");
+void sendSignal(int s) {
+	Serial.write(s);
 }
 
-/*
 signal receiveSignal(void) {
-	enum signal sig;
-	
 	if(Serial.available()) {
-		sig = Serial.read();
-		Serial.println('Receiving Signal!');
-		// char x = Serial.read();
+		return Serial.read();;
 	} else {
-		sig = NONE;
-	}
-	Serial.read(); // read in newline
-	return sig;
-} */
-
-
-// Alternate receiveSignal implementation
-
-signal receiveSignal(void) {
-	int signal;
-	while(Serial.available()) {
-		signal = Serial.readStringUntil('\n');
-		signal.trim();
+		return NONE;
 	}
 }
